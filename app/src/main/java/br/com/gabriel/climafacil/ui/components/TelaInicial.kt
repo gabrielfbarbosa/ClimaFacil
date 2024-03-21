48386d737d690106236e62082a26edc640bb4e16
gabrielfbarbosa <gfelipebarbosa@yahoo.com>
2024-03-20 22:02:36 -0300

    Criando layout

---
+++ app/src/main/java/br/com/gabriel/climafacil/ui/components/TelaInicial.kt
@@ -0,0 +1,18 @@
+package br.com.gabriel.climafacil.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun CorFundo() {
+    Column {
+
+    }
+}
+
+@Preview
+@Composable
+private fun CorFundoPreview() {
+    
+}
